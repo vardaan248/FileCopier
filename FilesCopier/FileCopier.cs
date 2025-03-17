a62afaeca5647c80123e35b19b3a3658f1bcdede
SETHI Vardaan <vardaan.sethi@hexagon.com>
2025-03-17 15:02:30 +0530

    Initial commit

---
+++ FilesCopier/FileCopier.cs
@@ -0,0 +1,39 @@
+﻿namespace FilesCopier
+{
+    public class FileCopier
+    {
+        public static void CopyFilesToTargetSize(string sourceFile, long targetSize, string outputDir, string outputFileExtension)
+        {
+            // Creating directory if it is not there
+            Directory.CreateDirectory(outputDir);
+
+            FileInfo sourceFileInfo = new(sourceFile);
+            long sourceFileSize = sourceFileInfo.Length;
+
+            // Source file size must not larger than the target size
+            if (sourceFileSize > targetSize)
+            {
+                Console.WriteLine("Source file is larger than the target size.");
+                return;
+            }
+
+            int outputFileCounter = 1;
+            long totalSize = sourceFileSize;
+
+            // Repeat the process of copying the file until total size reaches the target size
+            while (totalSize < targetSize)
+            {
+                string outputFilePath = Path.Combine(outputDir, $"F{outputFileCounter}${outputFileExtension}");
+
+                // Copy the source file to the new output file
+                File.Copy(sourceFile, outputFilePath);
+
+                totalSize += sourceFileSize; // Update the total size
+
+                outputFileCounter++; // Increment the counter for the next file
+            }
+
+            Console.WriteLine($"File splitting completed. Total files created: {outputFileCounter - 1}");
+        }
+    }
+}
---
+++ FilesCopier/Program.cs
@@ -0,0 +1,25 @@
+﻿using FilesCopier;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+
+        // List of source files to be copied
+        string sourceFile = "D:\\DriveboxPOC\\MainFolder3\\F1.txt";
+
+        // Define the target size for each output file (in bytes)
+        long targetSize = 5 * 1024; // 5 KB target size
+
+        // Define the directory where output files will be saved
+        string outputDir = "D:\\DriveboxPOC\\MainFolder3\\OutputFiles";
+
+        // Define the output file extension
+        string outputFileExtension = ".txt";
+
+        // Call the method to copy files to the target size
+        FileCopier.CopyFilesToTargetSize(sourceFile, targetSize, outputDir, outputFileExtension);
+    }
+}
